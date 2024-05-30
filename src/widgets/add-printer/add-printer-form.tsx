e7ce21557e30d9a46ab4012941a0a61cb2cddf64
Trekusov Max <maxtrekusovsite@gmail.com>
2024-05-30 16:58:27 +0300

    добавлены поля для добавления МФУ

--- src/widgets/add-printer/add-printer-form.tsx
+++ src/widgets/add-printer/add-printer-form.tsx
@@ -1,7 +1,7 @@
 import { useState } from 'react'
 import { storage } from '@/shared/config/firebase/firebase-config'
 import { InboxOutlined } from '@ant-design/icons'
-import { Button, Flex, Form, Progress, Space, Upload, message } from 'antd'
+import { Button, Flex, Form, Input, Progress, Select, Space, Upload, message } from 'antd'
 import { RcFile, UploadChangeParam, UploadFile } from 'antd/es/upload'
 import { UploadTaskSnapshot, getDownloadURL, ref, uploadBytesResumable } from 'firebase/storage'
 
@@ -9,6 +9,7 @@ type ProgressStatuses = 'normal' | 'exception' | 'active' | 'success'
 
 export const AddPrinterForm = () => {
   const [progress, setProgress] = useState(0)
+  const [showProgress, setShowProgress] = useState(false)
   const [uploadStatus, setUploadStatus] = useState<ProgressStatuses>('active')
   const [fileUpload, setFileUpload] = useState<File | null>(null)
 
@@ -21,15 +22,15 @@ export const AddPrinterForm = () => {
   }
 
   const onFinish = () => {
-    //console.log('Received values of form: ', values)
     if (fileUpload) {
+      setShowProgress(true)
       const imageRef = ref(storage, `printers/ ${fileUpload.name}`)
 
       const uploadTask = uploadBytesResumable(imageRef, fileUpload)
       uploadTask.on(
         'state_changed',
         (snapshot: UploadTaskSnapshot) => {
-          setProgress((snapshot.bytesTransferred / snapshot.totalBytes) * 100)
+          setProgress(Math.round((snapshot.bytesTransferred / snapshot.totalBytes) * 100))
 
           //console.log('Upload is ' + progress + '% done')
           switch (snapshot.state) {
@@ -52,20 +53,30 @@ export const AddPrinterForm = () => {
           setUploadStatus('success')
         },
       )
-      /* .then((e) => {
-          console.log(e)
-          message.success('Image uploaded')
-        })
-        .catch((e) => message.error(e)) */
     }
   }
-  const onSubmit = () => {}
+  const onChange = (info: UploadChangeParam<UploadFile<any>>) => {
+    const { status } = info.file
+
+    if (status === 'removed') {
+      setFileUpload(null)
+    }
+  }
+
+  const onReset = () => {
+    setShowProgress(false)
+    setFileUpload(null)
+  }
 
   return (
-    <Form name="validate_other" onFinish={onFinish}>
+    <Form name="printerInfo" onFinish={onFinish}>
       <Flex vertical gap="middle" align="center" style={{ width: '100%' }}>
         <Form.Item>
-          <Form.Item name="dragger" valuePropName="fileList" getValueFromEvent={normFile} noStyle>
+          <Form.Item
+            name="printerImage"
+            valuePropName="fileList"
+            getValueFromEvent={normFile}
+            noStyle>
             <Upload.Dragger
               style={{ display: fileUpload?.size ? 'none' : '', width: '100%' }}
               name="uploadFile"
@@ -77,16 +88,7 @@ export const AddPrinterForm = () => {
                 setFileUpload(file)
                 return false
               }}
-              onChange={(info: UploadChangeParam<UploadFile<any>>) => {
-                const { status } = info.file
-
-                if (status !== 'uploading') {
-                  console.log(info.file)
-                }
-                if (status === 'removed') {
-                  setFileUpload(null)
-                }
-              }}>
+              onChange={(info) => onChange(info)}>
               <p className="ant-upload-drag-icon">
                 <InboxOutlined />
               </p>
@@ -95,16 +97,43 @@ export const AddPrinterForm = () => {
             </Upload.Dragger>
           </Form.Item>
         </Form.Item>
-        <Flex align="center" style={{ width: '25%' }}>
+        <Flex
+          align="center"
+          style={{
+            width: '25%',
+            display: showProgress ? 'flex' : 'none',
+          }}>
           <Progress percent={progress} size="default" status={uploadStatus} />
+        </Flex>
+        <Flex align="center" justify="space-evenly" vertical>
+          <Form.Item label="Наименование" style={{ width: 350 }}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="Серийный номер" style={{ width: 350 }}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="Учётный номер Xerox" style={{ width: 350 }}>
+            <Input />
+          </Form.Item>
+          <Form.Item label="IP адрес" style={{ width: 350 }}>
+            <Input />
+          </Form.Item>
+
+          <Form.Item label="Офис">
+            <Select style={{ width: 300 }}>
+              <Select.Option value="dmitrov">Дмитров</Select.Option>
+              <Select.Option value="sposad">Сергиев Посад</Select.Option>
+              <Select.Option value="taldom">Талдом</Select.Option>
+            </Select>
+          </Form.Item>
         </Flex>
         <Flex align="center" justify="space-between">
           <Space>
-            <Button type="primary" htmlType="submit" onClick={() => onSubmit}>
-              Submit
+            <Button disabled={fileUpload ? false : true} type="primary" htmlType="submit">
+              Добавить
             </Button>
-            <Button htmlType="reset" onClick={() => setFileUpload(null)}>
-              reset
+            <Button htmlType="reset" onClick={onReset}>
+              Сброс
             </Button>
           </Space>
         </Flex>
