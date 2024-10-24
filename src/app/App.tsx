4e01412f08d41705d4a49e21914d6cff7fca9183
Trekusov Max <maxtrekusovsite@gmail.com>
2024-10-24 13:24:35 +0300

    добавлен fallbask element

--- src/app/App.tsx
+++ src/app/App.tsx
@@ -5,11 +5,12 @@ import { Providers } from './providers'
 import '@/shared/config/firebase/firebase-config.js'
 import { RouterProvider } from 'react-router-dom'
 import { router } from './providers/routes-provider/router'
+import { Loader } from '@/shared/ui/loader'
 
 const App: React.FC = () => {
   return (
     <Providers>
-      <RouterProvider router={router} />
+      <RouterProvider fallbackElement={<Loader />} router={router} />
     </Providers>
   )
 }
